d03ad27a3e29b14854ade431c00fb9a34ec88652
@douglasabnovato <douglasabnovato@gmail.com>
2020-03-20 23:51:27 -0300

    componente formulario - FormTodo

--- vuejs-cli/src/components/Comments.vue
+++ vuejs-cli/src/components/Comments.vue
@@ -1,21 +1,12 @@
 <template>
     <div class="container">
         <h1>Comentários</h1>
-        <hr/>
-        <div class="form-todo form-group">
-            <p>
-                <input placeholder="Nome" v-model="name" type="text" name="author" class="form-control"/>                    
-            </p>
-            <p>
-                <textarea placeholder="Comentário" v-model="message" name="message" class="form-control"></textarea>
-            </p>
-            <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
-            <hr/>
-        </div>
+        <hr/>   
+        <FormTodo v-on:add-todo="addComment"></FormTodo> 
         <div class="list-group">
             <div class="list-group-item" v-for="(comment, index) in allComments">
-                <span class="comment_author">Autor: <strong>{{comment.name}}</strong></span>
-                <p>{{comment.message}}</p>
+                <span class="comment__author">Autor: <strong>{{ comment.name }}</strong></span>
+                <p>{{ comment.message }}</p>
                 <div>
                     <a href="#" v-on:click.prevent="removeComment(index)" title="Excluir">Excluir</a>
                 </div>
@@ -26,28 +17,20 @@
 </template>
 
 <script>
+    import FormTodo from './FormTodo';
    export default{
+    components:{
+        FormTodo
+    },
     data(){
         return{
-            comments:[],
-            name:'',
-            message:''
+            comments:[]
         }
     },
-    methods:{
+    methods:{ 
         addComment(){
-            if(this.message.trim() === ''){
-                return;
-            }
-
-            this.comments.push({
-                name: this.name,
-                message: this.message
-            });
-
-            this.name = '',
-            this.message = ''
-        },
+            this.comments.push(comment);
+        },            
         removeComment(index){
             this.comments.splice(index, 1);
         }
@@ -61,9 +44,13 @@
         }
     },
     watch:{
-        comments(){
-
+        comments(val){
+            console.log('val', val)
         }
     }
   }
 </script>      
+
+<style>
+
+</style>
---
+++ vuejs-cli/src/components/FormTodo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="form-todo form-group">
+        <p>
+            <input placeholder="Nome" v-model="name" type="text" name="author" class="form-control"/>                    
+        </p>
+        <p>
+            <textarea placeholder="Comentário" v-model="message" name="message" class="form-control"></textarea>
+        </p>
+        <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
+        <hr/>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                name: '',
+                message:''
+            }
+        },
+        methods:{
+            addComment(){
+                if(this.message.trim() === ''){
+                    return;
+                }
+
+                this.$emit('add-todo', {
+                    name: this.name,
+                    message:this.message,
+                });
+
+                this.name = '',
+                this.message = ''
+            }
+        }
+    }
+</script>
